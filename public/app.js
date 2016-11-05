662248d599f6bfcfd89507af41805238c1b480a5
Ramiro Lopez <Ramiro@Ramiros-MacBook-Pro.local>
2016-11-05 01:59:42 -0700

    changes

---
+++ public/app.js
@@ -0,0 +1,7 @@
+// Notice the value that appears after MONGODB_URI =>. This is your URI string. Copy it to a document for safekeeping.
+
+// When you’re ready to connect Mongoose with your remote database, simply paste the URI string as the lone argument 
+// of your mongoose.connect()function. That’s it!
+
+// MONGODB_URI: mongodb:
+//heroku_xkk7sh28:rp3ddrl5ue0a1od0vle1ra28fv@ds145677.mlab.com:45677/heroku_xkk7sh28
